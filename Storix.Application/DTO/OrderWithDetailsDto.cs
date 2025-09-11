3fe60f911881b1ccf9e88ebe9ff1f5923d06ffa4
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-09-11 01:04:22 +0000

    Other DTOs

---
+++ Storix.Application/DTO/OrderWithDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿using Storix.Application.DTO.Order;
+
+namespace Storix.Application.DTO
+{
+    public class OrderWithDetailsDto:OrderDto
+    {
+        public string? SupplierName { get; set; }
+        public string? CustomerName { get; set; }
+        public string CreatedByName { get; set; } = string.Empty;
+        public decimal TotalAmount { get; set; }
+        public int TotalItems { get; set; }
+    }
+}
---
+++ Storix.Application/DTO/ProductWithDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using Storix.Application.DTO.Product;
+
+namespace Storix.Application.DTO
+{
+    public class ProductWithDetailsDto:ProductDto
+    {
+        public string SupplierName { get; set; } = string.Empty;
+        public string CategoryName { get; set; } = string.Empty;
+        public int TotalStock { get; set; }
+        public int AvailableStock { get; set; }
+    }
+}
