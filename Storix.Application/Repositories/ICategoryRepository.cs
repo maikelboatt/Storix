231d120b8d476ebcb14c2005b49ede309aa9c7bf
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-09-25 15:04:17 +0000

    Interface for category repos

---
+++ Storix.Application/Repositories/ICategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Storix.Domain.Models;
+
+namespace Storix.Application.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Category?> GetByIdAsync( int categoryId );
+
+        Task<IEnumerable<Category>> GetAllAsync();
+
+        Task<IEnumerable<Category>> GetRootCategoriesAsync();
+
+        Task<IEnumerable<Category>> GetSubCategoriesAsync( int parentCategoryId );
+
+        Task<Category> CreateAsync( Category category );
+
+        Task<Category> UpdateAsync( Category category );
+
+        Task<bool> DeleteAsync( int categoryId );
+
+        Task<bool> ExistsAsync( int categoryId );
+
+        Task<bool> NameExistsAsync( string name, int? excludeCategoryId = null );
+
+        Task<int> GetTotalCountAsync();
+
+        Task<IEnumerable<Category>> GetPagedAsync( int pageNumber, int pageSize );
+    }
+}
