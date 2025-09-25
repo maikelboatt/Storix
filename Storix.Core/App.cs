19a1e4d92fb3d53b37f5b5c61c131e3500d6a7c5
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-09-25 14:50:52 +0000

    Add application entry point and setup for dependency injection and logging

---
+++ Storix.Core/App.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.ViewModels;
+using Storix.Core.ViewModels;
+
+namespace Storix.Core
+{
+    public class App:MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<ShellViewModel>();
+        }
+    }
+}
---
+++ Storix.Presentation/Setup.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MvvmCross.IoC;
+using MvvmCross.Platforms.Wpf.Core;
+using Serilog;
+using Serilog.Extensions.Logging;
+using Storix.Infrastructure;
+
+namespace Storix.Presentation
+{
+    public class Setup:MvxWpfSetup<Core.App>
+    {
+        private IConfigurationRoot _configuration;
+
+        protected override ILoggerProvider? CreateLogProvider() => new SerilogLoggerProvider();
+
+        protected override ILoggerFactory? CreateLogFactory()
+        {
+            _configuration = new ConfigurationBuilder()
+                             .SetBasePath(Directory.GetCurrentDirectory())
+                             .AddJsonFile("appsettings.json", false, true)
+                             .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                         .ReadFrom.Configuration(_configuration)
+                         .CreateLogger();
+
+            return new SerilogLoggerFactory();
+        }
+
+        protected override void InitializeFirstChance( IMvxIoCProvider iocProvider )
+        {
+            base.InitializeFirstChance(iocProvider);
+
+            CompositionRoot.RegisterAllDependencies(iocProvider, _configuration);
+        }
+    }
+}
