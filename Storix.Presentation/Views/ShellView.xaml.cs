32e1439fbdba3a7989703af014b358f54a1b572a
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-09-25 15:02:58 +0000

    ShellView UI and ViewModel

---
+++ Storix.Presentation/Views/ShellView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using MvvmCross.Platforms.Wpf.Views;
+
+namespace Storix.Presentation.Views
+{
+    public partial class ShellView:MvxWpfView
+    {
+        private Window? _parentWindow;
+
+        public ShellView()
+        {
+            _parentWindow = Window.GetWindow(this);
+            InitializeComponent();
+        }
+
+        private void ShellView_OnLoaded( object sender, RoutedEventArgs e )
+        {
+            _parentWindow = Window.GetWindow(this);
+        }
+    }
+}
