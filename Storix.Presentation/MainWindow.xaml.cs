e3276b5bb1c103615ef41b3604e3435c7e538ceb
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-09-25 15:05:46 +0000

    Refactor MainWindow to inherit from MvxWindow and update XAML properties

--- Storix.Presentation/MainWindow.xaml.cs
+++ Storix.Presentation/MainWindow.xaml.cs
@@ -1,23 +1,15 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using MvvmCross.Platforms.Wpf.Views;
 
-namespace Storix.Presentation;
-
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow:Window
+namespace Storix.Presentation
 {
-    public MainWindow()
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow:MvxWindow
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
     }
 }
