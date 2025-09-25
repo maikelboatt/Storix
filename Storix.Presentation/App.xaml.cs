6074b202c54752d8b2c4b08d9fe6aa44f2187aa6
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-09-25 14:51:46 +0000

    Refactor application entry point to use MvvmCross framework and implement startup logic

--- Storix.Presentation/App.xaml.cs
+++ Storix.Presentation/App.xaml.cs
@@ -1,12 +1,24 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using MvvmCross.Core;
+using MvvmCross.Platforms.Wpf.Views;
 
-namespace Storix.Presentation;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App:Application
+namespace Storix.Presentation
 {
+    /// <summary>
+    ///     Interaction logic for App.xaml
+    /// </summary>
+    public partial class App:MvxApplication
+    {
+        protected override void RegisterSetup()
+        {
+            this.RegisterSetupType<Setup>();
+        }
+
+        protected override void OnStartup( StartupEventArgs e )
+        {
+            base.OnStartup(e);
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+        }
+    }
 }
