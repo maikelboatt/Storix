13834ff891709f7429d9731d66c10d72180ad129
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-10-29 23:39:32 +0000

    Add modal navigation store and view model factory interfaces for managing modal view models

---
+++ Storix.Core/Factory/IViewModelFactory.cs
@@ -0,0 +1,11 @@
+﻿using MvvmCross.ViewModels;
+
+namespace Storix.Core.Factory
+{
+    public interface IViewModelFactory
+    {
+        TViewModel? CreateViewModel<TViewModel>() where TViewModel : MvxViewModel;
+
+        TViewModel? CreateViewModel<TViewModel, TParameter>( TParameter parameter ) where TViewModel : MvxViewModel;
+    }
+}
