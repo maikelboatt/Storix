21fe0db2916b4244d1c4cb75a486bfe32d0eb975
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-11-06 22:40:40 +0000

    Analytics Pane now displays the DashboardView

--- Storix.Core/ViewModels/AnalyticsPaneViewModel.cs
+++ Storix.Core/ViewModels/AnalyticsPaneViewModel.cs
@@ -5,5 +5,14 @@ namespace Storix.Core.ViewModels
 {
     public class AnalyticsPaneViewModel( IViewModelFactory viewModelFactory ):MvxViewModel
     {
+        public DashboardViewModel? Dashboard => CreateDashboardViewModel();
+
+        private DashboardViewModel? CreateDashboardViewModel()
+        {
+            DashboardViewModel? viewModel = viewModelFactory.CreateViewModel<DashboardViewModel>();
+            viewModel?.Prepare();
+            viewModel?.Initialize();
+            return viewModel;
+        }
     }
 }
