ecd3e9713b2e515ec01d2e6306e248876e1c7940
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-11-25 17:05:39 +0000

    OrderDeleteViewModelBase definition for deleting orders

---
+++ Storix.Core/ViewModels/Orders/OrderDeleteViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using MvvmCross.ViewModels;
+using Storix.Application.Services.Orders.Interfaces;
+using Storix.Application.Stores.Orders;
+
+namespace Storix.Core.ViewModels.Orders
+{
+    public class OrderDeleteViewModelBase:MvxViewModel<int>
+    {
+        private int _orderId;
+
+        public OrderDeleteViewModelBase(IOrderService orderService, IOrderStore orderStore)
+        {
+            
+        }
+
+        public override void Prepare( int parameter )
+        {
+            _orderId = parameter;
+        }
+    }
+}
