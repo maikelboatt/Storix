7210cc94b0ae06f3e3efdf81144956e24313e5e4
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2026-01-08 22:38:30 +0000

    Dialog Service for displaying Error, Warning, Informartion and Confirmation

---
+++ Storix.Application/Services/Dialog/DialogService.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+
+namespace Storix.Application.Services.Dialog
+{
+    public class DialogService:IDialogService
+    {
+        public void ShowError( string message, string caption = "Error" )
+        {
+            MessageBox.Show(
+                message,
+                caption,
+                MessageBoxButton.OK,
+                MessageBoxImage.Error);
+        }
+
+        public void ShowWarning( string message, string caption = "Warning" )
+        {
+            MessageBox.Show(
+                message,
+                caption,
+                MessageBoxButton.OK,
+                MessageBoxImage.Warning);
+        }
+
+        public void ShowInfo( string message, string caption = "Information" )
+        {
+            MessageBox.Show(
+                message,
+                caption,
+                MessageBoxButton.OK,
+                MessageBoxImage.Information);
+        }
+
+        public bool ShowConfirmation( string message, string caption = "Confirm" )
+        {
+            MessageBoxResult result = MessageBox.Show(
+                message,
+                caption,
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Question);
+            return result == MessageBoxResult.Yes;
+        }
+    }
+}
---
+++ Storix.Application/Services/Dialog/IDialogService.cs
@@ -0,0 +1,13 @@
+﻿namespace Storix.Application.Services.Dialog
+{
+    public interface IDialogService
+    {
+        void ShowError( string message, string caption = "Error" );
+
+        void ShowWarning( string message, string caption = "Warning" );
+
+        void ShowInfo( string message, string caption = "Information" );
+
+        bool ShowConfirmation( string message, string caption = "Confirm" );
+    }
+}
